10f0cb7453c5fcdc2b1f3a469ed8358ba473d690
liwei <liwei@ijiami.local>
2024-04-29 14:17:16 +0800

    JWT 初步提交

--- src/main/java/com/coin/info/controller/AuthController.java
+++ src/main/java/com/coin/info/controller/AuthController.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class AuthController {
-    @Autowired
+    @Resource
     private AuthenticationManager authenticationManager;
 
     @Autowired
