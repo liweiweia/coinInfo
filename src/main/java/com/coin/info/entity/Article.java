99036daf9bd1d6db1459610d3ec080dad1e3d7a5
liwei <18046050640@163.com>
2024-05-16 11:02:48 +0800

    新增文章表字段

--- src/main/java/com/coin/info/entity/Article.java
+++ src/main/java/com/coin/info/entity/Article.java
@@ -7,8 +7,16 @@ import java.time.LocalDateTime;
 public class Article {
     private Long id;
     private String title;
+    //英文标题
+    private String enTitle;
+    //法语标题
+    private String frTitle;
     private String img;
     private String content;
+    //英文内容
+    private String enContent;
+    //法语内容
+    private String frContent;
     private Long authorId;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
